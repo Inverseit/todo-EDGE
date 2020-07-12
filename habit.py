02c813bc7aacec47ae16a581da9b6568614a22ce
Krymore <Seitkalievulan@gmail.com>
2020-07-12 13:32:26 +0600

    print

--- habit.py
+++ habit.py
@@ -16,6 +16,7 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
+    print("get message.text")
     bot.reply_to(message, message.text)
 
 
