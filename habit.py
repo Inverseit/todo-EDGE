b8593f8fa8de73bd4204e4bf1e80517d1baf687a
Krymore <Seitkalievulan@gmail.com>
2020-07-17 14:14:04 +0600

    check next step

--- habit.py
+++ habit.py
@@ -18,9 +18,9 @@ class Task:
 
 tasks = []
 
-def generate_markup(answers):
+def generate_markup(list):
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    for item in answers:
+    for item in list:
         markup.add(item)
     return markup
 
@@ -56,12 +56,12 @@ def process_change_motiv_step(message):
 def start_message(message):
     if len(tasks)!=0:
         m = bot.send_message(message.chat.id, "Please, choose habit:")
-        task_names_list = []
+        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         for task in tasks:
             bot.send_message(message.chat.id, "Here is '{}'".format(task.name))
             # print(task.name)
-            task_names_list.append(task.name)
-        bot.register_next_step_handler(m, process_show_step, reply_markup=generate_markup(task_names_list))
+            markup.add(task.name)
+        bot.register_next_step_handler(m, process_show_step, reply_markup=markup)
     else:
         bot.send_message(message.chat.id, "😱Please add habits by entering \start")
 def process_show_step(message):
