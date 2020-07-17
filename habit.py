ad64e705f223dd3c052be08bd6cd1701f6292cc0
Krymore <Seitkalievulan@gmail.com>
2020-07-17 12:30:40 +0600

    check next step

--- habit.py
+++ habit.py
@@ -7,7 +7,7 @@ TOKEN = '1125338216:AAFCOL_6RJYDPaSNNJEd3QAazK8yPUlNFDo'
 server = Flask(__name__)
 print("here")
 
-bot = telebot.TeleBot('1125338216:AAFCOL_6RJYDPaSNNJEd3QAazK8yPUlNFDo')
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -17,13 +17,47 @@ def start_message(message):
 def send_text(message):
     print(message.text.lower() + " is revieved")
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+        msg = bot.send_message(message.chat.id, 'Привет, мой создатель')
+        bot.register_next_step_handler(msg, process_name_step)
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
+    def process_name_step(message):
+        bot.send_message(message.chat.id, 'Проверяю следующие возможности')
 
 @bot.message_handler(content_types=['photo'])
 def send_text(message):
     bot.send_message(message.chat.id, 'красиво!')
+
+
+# @bot.message_handler(commands=['help', 'start'])
+# def send_welcome(message):
+#     msg = bot.reply_to(message, """\
+# Hi there, I am Example bot.
+# What's your name?
+# """)
+#     bot.register_next_step_handler(msg, process_name_step)
+  
+  
+# def process_name_step(message):
+#     try:
+#         chat_id = message.chat.id
+#         name = message.text
+#         user = User(name)
+#         user_dict[chat_id] = user
+#         msg = bot.reply_to(message, 'How old are you?')
+#         bot.register_next_step_handler(msg, process_age_step)
+#     except Exception as e:
+# bot.reply_to(message, 'oooops')
+
+
+
+
+
+
+
+
+
+
 
 
 
