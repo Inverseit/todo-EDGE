2fb76168b9c2492d7d41658d4954d3aaf9dfe743
Krymore <Seitkalievulan@gmail.com>
2020-07-17 14:26:30 +0600

    check next step

--- habit.py
+++ habit.py
@@ -19,7 +19,7 @@ class Task:
 tasks = []
 
 def generate_markup(list):
-    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup = telebot.types.InlineKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     for item in list:
         markup.add(item)
     return markup
