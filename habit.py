eaec872597fa82fcbd8929cbb3349ddd691b383d
Krymore <Seitkalievulan@gmail.com>
2020-07-19 22:07:54 +0600

    t

--- habit.py
+++ habit.py
@@ -26,7 +26,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Hey let's get started!") # add some text
-    bot.send_message(message.chat.id, "`\add` To add tasks to your list <br> `\show` To see all your tasks  <br> `\help` To understand how to use this bot", parse_mode='HTML') # add some text
+    bot.send_message(message.chat.id, "\add To add tasks to your list \n \show To see all your tasks  \n \help To understand how to use this bot") # add some text
 
 
 
