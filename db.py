fb07ca123c6e712454a36c17a4914f1139bc05c0
Krymore <Seitkalievulan@gmail.com>
2020-07-20 23:51:42 +0600

    dbtest with some implem

--- db.py
+++ db.py
@@ -49,6 +49,22 @@ def update(chat_id, task, status):
     conn.close()
     return tasks
 
+def delete(chat_id, task, status):
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    command_update = """UPDATE tasks SET status={0} WHERE chat_id = '{1}' AND task = '{2}');""".format(status, str(chat_id), task)
+    cur.execute(command_insert)
+    command = """SELECT task, status FROM tasks WHERE chat_id='{}';""".format(str(chat_id))
+    cur.execute(command)
+    records = cur.fetchall()
+    cur.close()
+    tasks = {}
+    for s in records:
+        task_name, status  = s
+        tasks[task_name] = status
+    conn.close()
+    return tasks
+
 
 # CREATE TABLE tasks (
 #     task_id SERIAL PRIMARY KEY,
