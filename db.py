f0c620a37c35d271f480aa569795a0e432a85ba5
Krymore <Seitkalievulan@gmail.com>
2020-07-20 22:38:00 +0600

    Add changing icons as help suggests

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
-test_command =  """INSERT INTO tasks (chat_id, task, status) VALUES ("123", "nts", 0);"""
+test_command =  """INSERT INTO tasks (chat_id, task, status) VALUES ('123', 'nts', 0);"""
 cur.execute(test_command)
 cur.close()
 
