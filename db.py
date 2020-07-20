9212af2de62337115d3d6e315d3d175706a7ec40
Krymore <Seitkalievulan@gmail.com>
2020-07-20 22:59:17 +0600

    dbtest

--- db.py
+++ db.py
@@ -6,13 +6,16 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 # test_command =  """INSERT INTO tasks (chat_id, task, status) VALUES ('12345', 'nts11', 1);"""
-test_command1 = """SELECT * FROM tasks;"""
+test_command1 = """SELECT task, status FROM tasks WHERE chat_id='123';"""
 cur.execute(test_command1)
 records = cur.fetchall()
-for s in records:
-    print(s)
 cur.close()
-
+tasks = {}
+for s in records:
+    task_name, status  = s
+    tasks[task_name] = status
+for key, value in tasks.items():
+    print("{0} has status {1}".format(key, value))
 conn.commit()
 conn.close()
 
